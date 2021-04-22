530b444cfe563e73e718f5ea4f70457f717d4b26
PuneetSivananda <psivananda@chatsoft.com>
2021-04-22 17:57:15 +0530

    feat: initial commit

---
+++ src/python_intro.py
@@ -0,0 +1,16 @@
+import numpy as np
+def reorder_list():
+    values = [7,2,34,8,11,23,6]
+    """Write Code below
+    """
+    print("Unsorted:", values)
+    print("Sorted:", sorted(values))
+    print("Numpy Sorted:", np.sort(values))
+    return
+
+reorder_list()
+
+
+"""
+Virtual Environment
+"""
