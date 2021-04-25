41e3137be545a56e2f791012a565b4f243fe0815
PuneetSivananda <psivananda@chatsoft.com>
2021-04-25 21:16:37 +0530

    sets functions

--- src/sets.py
+++ src/sets.py
@@ -1,2 +1,13 @@
 set_a = {"one", "two", "three"}
 print(set_a)
+set_a.add("four")
+print(set_a)
+
+set_a.update(["five", "six","seven" ])
+print(set_a)
+
+set_a.remove("six")
+print(set_a)
+
+set_a.discard("seven")
+print(set_a)
