aecbe16d1198047a6d3e9c9623f24253d1a5173c
PuneetSivananda <psivananda@chatsoft.com>
2021-04-25 21:25:20 +0530

    dictionarues

--- src/dictionsries.py
+++ src/dictionsries.py
@@ -1,4 +1,12 @@
 our_dictionary = {"key1":"value1",
 "key2":"value2"}
 
+person = {"name":"John doe", 
+"height":"7'5",
+"location":"Memphis"}
+
 print(our_dictionary)
+
+print(person)
+print(person["name"])
+print(person["height"])
