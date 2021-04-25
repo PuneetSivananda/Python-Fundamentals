a6ddffe0ad09fe153521f6cadee75967bb2502c8
PuneetSivananda <psivananda@chatsoft.com>
2021-04-25 21:21:11 +0530

    sets

--- src/sets.py
+++ src/sets.py
@@ -1,5 +1,7 @@
 set_a = {"one", "two", "three"}
 print(set_a)
+
+f_set_a = frozenset(set_a)
 set_a.add("four")
 print(set_a)
 
@@ -11,3 +13,9 @@ print(set_a)
 
 set_a.discard("seven")
 print(set_a)
+
+set_a.pop()
+print(set_a)
+
+
+print(f_set_a)
