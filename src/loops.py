0b763b98e06157919c88404c4f657005fdeca087
PuneetSivananda <psivananda@chatsoft.com>
2021-05-01 19:51:10 +0530

    loops

---
+++ src/loops.py
@@ -0,0 +1,25 @@
+x = 1
+y = 2 
+z = 3
+
+if x < y and y < z:
+  print("x is less than y")
+else:
+  print("Victory")
+
+if x < y and y > z:
+  print("x is less than y")
+else:
+  print("Victory")
+
+
+i = 1
+while i < 10:
+  print(i)
+  i = i+1
+  if i == 6:
+    break
+
+name = "FORLOOP"
+for i in name:
+  print(i)
